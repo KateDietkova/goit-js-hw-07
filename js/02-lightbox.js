cac74faf6c7a4b206b91229138ff165dfac0c1a2
Katerina Dietkova <katrinkir1296@gmail.com>
2022-10-03 21:59:07 +0300

    Add task 2 with SimplelightBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const imgMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.innerHTML = imgMarkup;
+
+galleryContainer.addEventListener('click', onImgOpen);
+
+const lightBox = new SimpleLightbox(".gallery a", { captionDelay: 250});
+
+function onImgOpen(event) {
+    event.preventDefault();
+}
+
+
+
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+            </a>
+            `;
+    })
+    .join("");
+}
+
+
