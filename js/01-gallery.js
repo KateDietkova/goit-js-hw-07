59351c2c1a6d5f0f11f2a1a3b83d08bcd21aed47
Katerina Dietkova <katrinkir1296@gmail.com>
2022-10-03 17:28:06 +0300

    Add task 1 with basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,74 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+// Create gallery
+const imgMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.innerHTML = imgMarkup;
+
+galleryContainer.addEventListener("click", onOpenModalImg);
+
+
+
+function onOpenModalImg(event) {
+  event.preventDefault();
+  const currentImg = event.target;
+
+  if (currentImg.nodeName !== "IMG") {
+    return;
+  }
+
+  const url = onGetOriginalUrl(currentImg);
+
+  generateModalImg(url);
+
+}
+
+// Get original url
+function onGetOriginalUrl(currentImg) {
+  return currentImg.dataset.source;
+}
+
+function generateModalImg(imgUrl) {
+  const instance = basicLightbox.create(
+    `
+        <img
+        src="${imgUrl}"
+        heigth = "800"
+        width = "600"
+        />
+    `
+  );
+
+    instance.show();
+
+    galleryContainer.addEventListener("keydown", (event) => {
+        if (event.code !== "Escape") {
+          return;
+        }
+
+        instance.close();
+    });
+}
+
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>
+            `;
+    })
+    .join("");
+}
